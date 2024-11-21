08b701bf52209e56254d7f67ab1d3ac50fb714de
giedre <giedre.baublyte@codeacademylt.onmicrosoft.com>
2024-11-21 19:34:02 +0200

    changed db file name

--- app/database.py
+++ app/database.py
@@ -3,7 +3,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base, relationship  # , relationship
 from datetime import datetime
 
-engine = create_engine('sqlite:///todo.db')
+DATABASE_URL = "sqlite:///./test.db"
+
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
