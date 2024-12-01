4e6b47fe37f83a6eff9a1551646ad365284bdad4
giedre <giedre.baublyte@codeacademylt.onmicrosoft.com>
2024-12-02 00:09:46 +0200

    changed to save main.db to app/main.db

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base, relationship
 
-DATABASE_URL = "sqlite:///./main.db"
+DATABASE_URL = "sqlite:///./app/main.db"
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
